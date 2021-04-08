263846a26dcff22cf18cdca38ee7209633cde5d3
zana-a <zana200.a@gmail.com>
2021-04-08 23:34:16 +0100

    add demos for enum

--- lib/target/enum.scala
+++ lib/target/enum.scala
@@ -9,9 +9,14 @@ object Application extends App {
 //
 object Enum {
 // counts the amount of items in the list.
-def count(list: List[Any], c: scala.Int): scala.Int = if (list.eq(List())) c
+def count(list: List[Any], c: scala.Int): scala.Int = if (Enum.empty(list)) c
 else count(Enum.drop(list, 1), c + 1)
 // drops the [n] amount of items of a list.
 def drop(list: List[Any], n: scala.Int): List[Any] = list.drop(n)
+def empty(list: List[Any]): scala.Boolean = list.eq(List())
 }
+val a_list = List("hello", " ", "world", "!")
+println(Enum.count(a_list, 0))
+println(Enum.drop(a_list, 1))
+println(Enum.empty(a_list))
 }
